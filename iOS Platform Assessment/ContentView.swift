582b9b60b8424f4fc277e7d93492235ccb3f5c44
byates-fleetian <byates@fleetio>
2025-04-16 08:35:58 -0500

    added sample app and README.md prompt

---
+++ iOS Platform Assessment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Platform Assessment
+//
+//  Created by Brandon Yates on 4/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Platform Assessment/iOS_Platform_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Platform_AssessmentApp.swift
+//  iOS Platform Assessment
+//
+//  Created by Brandon Yates on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Platform_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+          LoginView()
+        }
+    }
+}
